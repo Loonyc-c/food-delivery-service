2d433d03a5010af86c717a35ce12a67d9fa5921e
24LP7027 24LP7027 <24LP7027@G301-CC0200.local>
2025-02-24 17:59:14 +0800

    aa

--- src/controllers/sign-up-controller/createUser.controller.js
+++ src/controllers/sign-up-controller/createUser.controller.js
@@ -2,16 +2,26 @@ import { Users } from "../../schemas/users.schema.js"
 import bcrypt from "bcrypt"
 
 const createUsersController = async (req, res) => {
+    console.log(req.body)
     const { email,password } = req.body
     try {
 
+        const existingUser = await Users.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
         const bcryptedPassword =  bcrypt.hashSync(password, 8)
-        const newUser = await Users.create({
+
+        const createUser = await Users.create({
             email,
             bcryptedPassword
         })
-        res.send(newUser).status(200).JSON("sign up succesfully")
+
+        res.send(createUser).status(200).JSON("sign up succesfully")
     } catch (error) {
+
         console.log("error", error)
         res.send().status(200)
     }
--- src/schemas/users.schema.js
+++ src/schemas/users.schema.js
@@ -2,7 +2,10 @@ import mongoose, { mongo } from "mongoose"
 
 const usersSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    age: { type: Number, required: false }, 
+    passowrd: { type: String, required: true }, 
+    phoneNumber:{type:String},
+    location:{type:String},
+    
 })
 
 export const Users = mongoose.model("users",usersSchema)
