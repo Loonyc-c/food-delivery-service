470c6760a2b9e770a12a2d5bc6da9bcae86f8d05
Battulga <battulgabathuyg54@gmail.com>
2025-03-26 03:17:08 +0800

    update food

--- src/controllers/food.controller/updateFood.controller.js
+++ src/controllers/food.controller/updateFood.controller.js
@@ -2,24 +2,27 @@ import { FoodsModel } from "../../schemas/food.schema.js";
 import mongoose from "mongoose";
 
 const updateFoodById = async (req, res) => {
-    try {
-        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-            return res.status(400).json({ message: "Invalid food ID format" });
-        }
-        const { foodName, price, ingredients, category,image } = req.body;
-
-        const updatedFood = await FoodsModel.findByIdAndUpdate(
-            req.params.id,
-            { foodName, price, ingredients, category,image },
-        );
-        if (!updatedFood) {
-            return res.status(404).json({ message: "Food not found" });
-        }
-
-    } catch (error) {
-        console.log("Error updating food:", error);
-        res.status(500).json({ message: "Server error" })
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid food ID format" });
     }
-}
+    const { foodName, price, ingredients, category, image } = req.body;
 
-export default updateFoodById
+    const updatedFood = await FoodsModel.findByIdAndUpdate(req.params.id, {
+      foodName,
+      price,
+      ingredients,
+      category,
+      image,
+    });
+    if (!updatedFood) {
+      return res.status(404).json({ message: "Food not found" });
+    }
+    res.json(updatedFood);
+  } catch (error) {
+    console.log("Error updating food:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export default updateFoodById;
